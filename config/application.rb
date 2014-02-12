23a63c777f99c33e429a1d611eca2f9281359adc
Sylvain UTARD <sylvain.utard@gmail.com>
2014-02-11 20:21:17 -0800

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,18 @@ Bundler.require(:default, Rails.env)
 
 module Depstack
   class Application < Rails::Application
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      
+      
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
