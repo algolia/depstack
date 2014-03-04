a0e0d760e31920c220e7a3cb785d8e3c327553a1
Sylvain UTARD <sylvain.utard@gmail.com>
2014-03-03 21:35:15 -0800

    Fetch dependencies too.

--- app/models/library.rb
+++ app/models/library.rb
@@ -40,54 +40,73 @@ class Library < ActiveRecord::Base
     (repository_uri || homepage_uri).scan(/github\.com\/([^\/]+)\/([^\/]+)/).first
   end
 
-  def self.load!(manager, name)
+  def self.load!(manager, name, fast = false)
     manager_cd = Library.managers[manager.to_s]
     library = Library.find_or_initialize_by(manager_cd: manager_cd, name: name)
-    if library.new_record?
-      case manager_cd
-      when Library.rubygems
-        json = JSON.parse(open("http://rubygems.org/api/v1/gems/#{name}.json").read) rescue nil
-        if json
-          library.downloads = json['downloads']
-          library.platform = json['platform']
-          library.description = json['info']
-          library.homepage_uri = json['homepage_uri']
-          library.repository_uri = json['source_code_uri']
-        end
-      when Library.npm, Library.bower
-        json = JSON.parse(open("https://registry.npmjs.org/#{name}").read) rescue nil
-        if json
-          library.downloads = 0
-          library.platform = (json['versions'] || {}).detect { |version,spec| (spec['engines'] || {}).detect { |k,v| k['node'] } } ? 'node' : 'js' rescue 'js'
-          library.description = json['description']
-          library.homepage_uri = json['homepage']
-          library.repository_uri = json['repository'] && json['repository']['url']
-        end
-      when Library.composer
-        json = JSON.parse(open("https://packagist.org/packages/#{name}.json").read) rescue nil
-        if json
-          library.downloads = json['package']['downloads'] && json['package']['downloads']['total']
-          library.platform = 'php'
-          library.description = json['package']['description']
-          library.homepage_uri = nil
-          library.repository_uri = json['package']['repository']
-        end
-      when Library.pip
-        json = JSON.parse(open("https://pypi.python.org/pypi/#{name}/json").read) rescue nil
-        if json
-          library.downloads = json['info']['downloads']['last_month']
-          library.platform = 'python'
-          library.description = json['info']['summary']
-          library.homepage_uri = json['info']['home_page']
-          library.repository_uri = json['info']['project_url']
-        end
-      else
-        raise "Unknown manager: #{manager}"
-      end
-    end
+    return library if !library.new_record? && fast
     p(library) if library.new_record?
+    case manager_cd
+    when Library.rubygems
+      json = JSON.parse(open("http://rubygems.org/api/v1/gems/#{name}.json").read) rescue nil
+      if json
+        library.downloads = json['downloads']
+        library.platform = json['platform']
+        library.description = json['info']
+        library.homepage_uri = json['homepage_uri']
+        library.repository_uri = json['source_code_uri']
+        library.load_dependencies! (json['dependencies']['development'] or []).map { |r| { name: r['name'], requirement: r['requirements'], environment: :development } } +
+          (json['dependencies']['runtime'] or []).map { |r| { name: r['name'], requirement: r['requirements'], environment: :runtime } }
+      end
+    when Library.npm, Library.bower
+      json = JSON.parse(open("https://registry.npmjs.org/#{name}").read) rescue nil
+      if json
+        library.downloads = 0
+        library.platform = (json['versions'] || {}).detect { |version,spec| (spec['engines'] || {}).detect { |k,v| k['node'] } } ? 'node' : 'js' rescue 'js'
+        library.description = json['description']
+        library.homepage_uri = json['homepage']
+        library.repository_uri = json['repository'] && (json['repository'].is_a?(Array) ? json['repository'].first : json['repository'])['url']
+        last_version = json['dist-tags']['latest'] rescue 'N/A'
+        library.load_dependencies! ((json['versions'][last_version] || json['versions'].first || {})['devDependencies'] || []).map { |k, v| { name: k, requirement: v, environment: :development } } + 
+          ((json['versions'][last_version] || json['versions'].first || {})['dependencies'] || []).map { |k, v| { name: k, requirement: v, environment: :runtime } }
+      end
+    when Library.composer
+      json = JSON.parse(open("https://packagist.org/packages/#{name}.json").read) rescue nil
+      if json
+        library.downloads = json['package']['downloads'] && json['package']['downloads']['total']
+        library.platform = 'php'
+        library.description = json['package']['description']
+        library.homepage_uri = nil
+        library.repository_uri = json['package']['repository']
+        version = (json['package']['versions']['dev-master'] || json['package']['versions'].first.last) rescue {}
+        library.load_dependencies! (version['require'] || []).map { |k, v| { name: k, requirement: v, environment: :runtime } } +
+          (version['require-dev'] || []).map { |k, v| { name: k, requirement: v, environment: :runtime } }
+      end
+    when Library.pip
+      json = JSON.parse(open("https://pypi.python.org/pypi/#{name}/json").read) rescue nil
+      if json
+        library.downloads = json['info']['downloads']['last_month']
+        library.platform = 'python'
+        library.description = json['info']['summary']
+        library.homepage_uri = json['info']['home_page']
+        library.repository_uri = json['info']['project_url']
+        # TODO: The metadata available for packages on PyPI does not include
+        #       information about the dependencies.
+        # library.load_dependencies!
+      end
+    else
+      raise "Unknown manager: #{manager}"
+    end
     library.save!
     library
+  end
+
+  def load_dependencies!(deps)
+    save!
+    self.dependencies = deps.map do |dep|
+      library = Library.load!(manager, dep[:name], true)
+      Dependency.new(source_id: id, destination_id: library.id, environment: dep[:environment], requirement: dep[:requirement])
+    end
+    save!
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,23 @@
 require 'open-uri'
 
+fast = !ENV['fast'].nil?
+
 puts "Importing Rubygems"
 `gem list --remote`.split(/\n/).each do |line|
   name = line.split(/\s/).first
-  Library.load!(:rubygems, name)
+  Library.load!(:rubygems, name, fast)
 end
 
 puts "Importing NPM"
 JSON.parse(open('http://isaacs.iriscouch.com/registry/_all_docs').read)['rows'].each do |row|
   name = row['id']
-  Library.load!(:npm, name)
+  Library.load!(:npm, name, fast)
 end
 
 puts "Importing PIP"
 open("https://pypi.python.org/pypi?:action=index").read.scan(/href="\/pypi\/([^\/]+)\/[^"]+/).each do |s|
   name = s.first
-  Library.load!(:pip, name)
+  Library.load!(:pip, name, fast)
 end
 
 puts "Importing Composer"
@@ -25,7 +27,7 @@ loop do
   (doc / 'h1 a').each do |a|
     name = a.text()
     next if name == 'Packagist'
-    Library.load!(:composer, name)
+    Library.load!(:composer, name, fast)
   end
   next_link = (doc / 'nav a').last
   break if next_link.text() != 'Next'
