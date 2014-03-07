bb062cf8893dddd137762a1fd461b9b467fc3d7f
Sylvain UTARD <sylvain.utard@gmail.com>
2014-03-06 23:40:53 -0800

    Deployment scripts

--- Gemfile
+++ Gemfile
@@ -45,3 +45,6 @@ group :test do
   gem 'database_cleaner', '1.0.1'
   gem 'email_spec'
 end
+group :production do
+  gem 'mysql2'
+end
---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+require "bundler/capistrano"
+
+set :default_environment, {
+  'GIT_SSH' => '/home/prod/ssh-depstack.sh'
+}
+
+default_run_options[:pty] = true 
+
+# servers
+server 'c3-use-1.algolia.io', :app, :web, :db, :cron, primary: true
+
+# application
+set :application, "Depstack"
+set :deploy_to, "/var/www/depstack"
+set :user, "prod"
+set :use_sudo, false
+
+# repository
+set :local_repository, "."
+set :repository, "git@github.com:algolia/depstack.git"
+set :scm, :git
+set :deploy_via, :remote_cache
+set :branch, "master"
+set :git_shallow_clone, 1
+
+# keep 5 last releases
+set :keep_releases, 5
+after "deploy:update", "deploy:cleanup"
+
+# configuration
+desc "Copy in server specific configuration files" 
+task :copy_shared do
+  run "cp #{deploy_to}/shared/config/database.yml #{release_path}/config/"
+  run "cp #{deploy_to}/shared/config/application.yml #{release_path}/config/"
+end
+before "bundle:install", "copy_shared"
+
+desc "Restart Thin"
+namespace :deploy do
+  task :restart do
+    run "cd #{current_path} && bundle exec thin restart -C #{deploy_to}/shared/thin.yml"
+  end
+end
+
+# ugly workaround for bug https://github.com/capistrano/capistrano/issues/81
+before "deploy:assets:precompile", "bundle:install"
+
+# rvm
+require "rvm/capistrano"
+set :rvm_ruby_string, 'ruby-2.0.0-p353'
+
+# cron
+#set :whenever_command, "bundle exec whenever"
+#set :whenever_roles, [:cron]
+#require "whenever/capistrano"
