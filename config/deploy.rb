2f9a9646515a1ba6a0c0c949a525115dbc29f38d
Sylvain UTARD <sylvain.utard@gmail.com>
2014-03-10 23:31:47 -0700

    Enable whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,6 @@ require "rvm/capistrano"
 set :rvm_ruby_string, 'ruby-2.0.0-p353'
 
 # cron
-#set :whenever_command, "bundle exec whenever"
-#set :whenever_roles, [:cron]
-#require "whenever/capistrano"
+set :whenever_command, "bundle exec whenever"
+set :whenever_roles, [:cron]
+require "whenever/capistrano"
